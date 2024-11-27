eeca7fb18fdf6af1df9ddf0b7f054a2b67e339b8
Zurich <Zurich@IN3114>
2024-11-26 19:09:13 -0600

    Created all required controllers

---
+++ Controllers/CobroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PVI_ProyectoFinal.Controllers
+{
+    public class CobroController : Controller
+    {
+        // GET: Cobro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
